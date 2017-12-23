8f49ccc43d5bc95c0a8909f6007d36f75c45b7f0
Alex Gubbay <gusgub@hotmail.co.uk>
2017-12-23 22:56:59 +0000

    Tweak LEVEL OF INTEREST variables.

--- src/jvm/org/apache/storm/starter/data/LEVEL_OF_INTEREST.java
+++ src/jvm/org/apache/storm/starter/data/LEVEL_OF_INTEREST.java
@@ -3,9 +3,9 @@ package org.apache.storm.starter.data;
 //Enumeration of TfL level of interest levels with tuned variables for severity calculations.
 public enum LEVEL_OF_INTEREST{
     
-    High(3.0),
-    Medium(1.5),
-    Low(1.0);
+    High(2.8),
+    Medium(1.3),
+    Low(0.8);
     
     private double numVal;
     
